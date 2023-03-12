d17c93fad8bdc12d554307b5c1ad23c8da051a4c
Moon <1069656390@qq.com>
2023-03-12 17:43:32 +0800

    📃init:create some modules

---
+++ src/main/java/top/moon/imiao/pojo/ImiaoPojoApplication.java
@@ -0,0 +1,13 @@
+package top.moon.imiao.pojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImiaoPojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImiaoPojoApplication.class, args);
+    }
+
+}
